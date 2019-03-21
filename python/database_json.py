0fc5abdea01b4a8f540f845848ca06bb39f06c80
Dmitry Kostrov <dmitryk.dk@gmail.com>
2019-03-21 22:02:41 +0500

    Initial for python

---
+++ python/database_json.py
@@ -0,0 +1,101 @@
+import os, json
+from datetime import datetime, timedelta
+
+MAIN_FILE='trips.json'
+TODAY_CACHE=[]
+
+def init_trips():
+    get_today_trips()
+
+def get_trips_from_file(file_name):
+    try:
+        data = []
+        with open(file_name) as f:
+            data = json.loads(f.read())
+    except e:
+        print('%s' % e)
+    finally:
+        return data
+
+def get_trips(when):
+    return get_trips_from_file(when.strftime('%Y%m%d') + '.json')
+
+def get_today_trips():
+    global TODAY_CACHE
+    if TODAY_CACHE == []:
+        now = datetime.now()
+        TODAY_CACHE=get_trips(now)
+
+    return TODAY_CACHE
+
+def get_all_trips():
+    global TODAY_CACHE
+    if TODAY_CACHE == []:
+        TODAY_CACHE=get_trips_from_file(MAIN_FILE)
+
+    return TODAY_CACHE
+
+def get_tomorrow_trips():
+    tomorrow = datetime.now() + timedelta(days=1)
+    return get_trips(tomorrow)
+
+def update_trips(trips):
+    with open(MAIN_FILE, 'w') as f:
+        json.dump(trips, f, indent=4, sort_keys=False, ensure_ascii=False)
+
+def add_trip(trip):
+    TODAY_CACHE.append(trip)
+    update_trips(TODAY_CACHE)
+
+def join_trip(trip_id, day):
+    if trip_id >= len(TODAY_CACHE):
+        raise Exception('Invalid Trip ID')
+
+    trip = TODAY_CACHE[trip_id]
+    if len(trip['days']) <= day:
+        raise Exception('Invalid day number: %d' % day) 
+    
+    if trip['days'][day]['guests'] >= trip['days'][day]['seats']:
+        raise Exception('No more seats for this Trip')
+    trip['days'][day]['guests'] += 1
+
+    TODAY_CACHE[trip_id] = trip
+    update_trips(TODAY_CACHE)
+
+def left_trip(trip_id, day):
+    if trip_id >= len(TODAY_CACHE):
+        raise Exception('Invalid Trip ID')
+
+    trip = TODAY_CACHE[trip_id]
+    if len(trip['days']) <= day:
+        raise Exception('Invalid day number: %d' % day) 
+    
+    if trip['days'][day]['guests'] <= 0:
+        raise Exception('No more seats for this Trip')
+    trip['days'][day]['guests'] -= 1
+
+    TODAY_CACHE[trip_id] = trip
+    update_trips(TODAY_CACHE)
+
+def remove_trip(trip_id):
+    if trip_id >= len(TODAY_CACHE):
+        raise Exception('Invalid Trip ID')
+
+    del TODAY_CACHE[trip_id]
+    update_trips(TODAY_CACHE)
+
+def edit_trip(trip, trip_id):
+    if trip_id >= len(TODAY_CACHE):
+        raise Exception('Invalid Trip ID')
+    
+    # fix new guests number
+    prev_trip = TODAY_CACHE[trip_id]
+    for idx, day in enumerate(prev_trip['days']):
+        old_guests = int(day['guests'])
+        new_seats = trip['days'][idx]['seats']
+        if old_guests > new_seats:
+            old_guests = new_seats
+        trip['days'][idx]['guests'] = old_guests
+
+    TODAY_CACHE[trip_id] = trip
+    update_trips(TODAY_CACHE)
---
+++ python/server.py
@@ -0,0 +1,108 @@
+from bottle import post, route, run, template, request, static_file, error, redirect
+from database_json import init_trips, get_all_trips, join_trip, left_trip, add_trip, remove_trip, edit_trip
+
+@route('/')
+def index():
+    trips = get_all_trips()
+    #return template('<b>Hello {{name}}</b>!', name=name)
+    #return trips#get_tomorrow_trips()
+    return template('templates/index.html', trips=trips)
+    #, name=request.environ.get('REMOTE_ADDR'))
+
+@route('/<day>')
+def index():
+    trips = get_all_trips()
+    #return template('<b>Hello {{name}}</b>!', name=name)
+    #return trips#get_tomorrow_trips()
+    return template('templates/day.html', trips=trips, day=int(day))
+
+@route('/trip/<id>/<day>/join')
+def join(id, day):
+    join_trip(int(id), int(day))
+    redirect('/')
+
+@route('/trip/<id>/<day>/left')
+def join(id, day):
+    left_trip(int(id), int(day))
+    redirect('/')
+
+@route('/trip/<id>/remove')
+def remove(id):
+    remove_trip(int(id))
+    redirect('/')
+
+@route('/trip/new')
+def new():
+    #return template('<b>Hello {{name}}</b>!', name=name)
+    #return trips#get_tomorrow_trips()
+    trip = {'time': '18:10', 'delay': '10', 
+            'driver': '',
+            'to': '', 
+            'from': '', 
+            'desc': '', 
+            'days' : [ 
+                {'seats': 4, 'guests': 0},
+                {'seats': 4, 'guests': 0},
+                {'seats': 4, 'guests': 0},
+                {'seats': 4, 'guests': 0},
+                {'seats': 4, 'guests': 0},
+                {'seats': 0, 'guests': 0},
+                {'seats': 0, 'guests': 0}
+            ]
+        }
+    return template('templates/new.html', trip=trip, id=-1)
+
+@post('/trip/edit')
+def edit():
+    time   = request.forms.time
+    delay  = request.forms.delay
+    driver = request.forms.driver
+    desc   = request.forms.desc
+    to     = request.forms.to
+    frm    = request.forms.frm # from
+    seats0 = request.forms.seats0
+    seats1 = request.forms.seats1
+    seats2 = request.forms.seats2
+    seats3 = request.forms.seats3
+    seats4 = request.forms.seats4
+    seats5 = request.forms.seats5
+    seats6 = request.forms.seats6
+    id     = request.forms.id
+
+    if frm and to and driver and time:
+        trip = {'driver': driver, 'time': time, 'desc': desc, 
+            'to': to, 
+            'from': frm, 
+            'delay': delay,
+            'days' : [ 
+                {'seats': int(seats0), 'guests': 0},
+                {'seats': int(seats1), 'guests': 0},
+                {'seats': int(seats2), 'guests': 0},
+                {'seats': int(seats3), 'guests': 0},
+                {'seats': int(seats4), 'guests': 0},
+                {'seats': int(seats5), 'guests': 0},
+                {'seats': int(seats6), 'guests': 0}
+            ]
+        }
+        if id == '-1':
+            add_trip(trip)
+        else:
+            edit_trip(trip, int(id))
+    
+    redirect('/')
+
+@route('/trip/<id>/modify')
+def modify(id):
+    trips = get_all_trips()
+    return template('templates/new.html', trip=trips[int(id)], id=id)
+
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
+
+@error(505)
+def error505(error):
+    return 'Nothing here, sorry, hehe'
+
+init_trips()
+run(host='172.19.25.172', port=8585, debug=True)
