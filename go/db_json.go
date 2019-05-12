f38fd3280f8d6722d3997cf211f68193ea392fc1
Dmitry Kostrov <dmitryk.dk@gmail.com>
2019-05-12 12:09:47 +0500

    Add create/modify trips; join/left a trip; more REST way

--- go/db_json.go
+++ go/db_json.go
@@ -1,23 +1,152 @@
 package main
 
 import (
-    "io/ioutil"
-    "log"
-    "os"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
-var TRIPS_FILE string = "trips.json"
-//TODAY_CACHE=[]
-
-func get_all_trips() (data string) {
-    file, err := os.Open(TRIPS_FILE)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-
-
-  b, err := ioutil.ReadAll(file)
-  return string(b)
+type Day struct {
+	seats  int `json:"seats"`
+	guests int `json:"guests"`
 }
 
+type Trip struct {
+	to     string `json:"to"`
+	delay  string `json:"delay"`
+	desc   string `json:"desc"`
+	driver string `json:"driver"`
+	time   string `json:"time"`
+	from   string `json:"from"`
+	days   []Day  `json:"days"`
+}
+
+var TRIPS_FILE string = "trips.json"
+var TRIPS_CACHE_INITED = false
+var TRIPS_CACHE []map[string]interface{}
+
+func get_all_trips() (string, error) {
+	if TRIPS_CACHE_INITED {
+		var bolb, err = json.Marshal(TRIPS_CACHE)
+		return string(bolb), err
+	}
+
+	file, err := os.Open(TRIPS_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal(b, &TRIPS_CACHE)
+	//json.Unmarshal(b, &dat)
+	//fmt.Println(dat)
+
+	TRIPS_CACHE_INITED = true
+	return string(b), err
+}
+
+func get_trip(id int) (string, error) {
+	get_all_trips()
+	if len(TRIPS_CACHE) <= id {
+		return "", errors.New("Wrong ID")
+	}
+	var bolb, err = json.Marshal(TRIPS_CACHE[id])
+	return string(bolb), err
+}
+
+func save_trips() {
+	var bolb, _ = json.Marshal(TRIPS_CACHE)
+	var err = ioutil.WriteFile(TRIPS_FILE, bolb, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func delete_trip(id int) {
+	get_all_trips()
+	if len(TRIPS_CACHE) <= id {
+		return
+	}
+
+	TRIPS_CACHE = append(TRIPS_CACHE[:id], TRIPS_CACHE[id+1:]...)
+	save_trips()
+}
+
+func update_guest(id int, day int, upd int) {
+	get_all_trips()
+	if len(TRIPS_CACHE) <= id {
+		return
+	}
+
+	md, err := TRIPS_CACHE[id]["days"].([]interface{})
+	fmt.Println(err, md, md[day])
+	mg, err := md[day].(map[string]interface{})
+
+	g := mg["guests"].(float64)
+	mg["guests"] = g + (float64)(upd)
+	save_trips()
+}
+
+func add_trip(trip map[string]interface{}) {
+	get_all_trips()
+	TRIPS_CACHE = append(TRIPS_CACHE, trip)
+	save_trips()
+}
+
+func edit_trip(trip map[string]interface{}, id int) {
+	get_all_trips()
+	if len(TRIPS_CACHE) <= id {
+		return
+	}
+	TRIPS_CACHE[id] = trip
+	save_trips()
+}
+
+func get_new_trip() string {
+	return `{
+        "to": "To",
+        "delay": "10",
+        "desc": "Desc",
+        "driver": "Driver",
+        "time": "18:10",
+        "from": "From",
+        "days": [
+            {
+                "seats": 4,
+                "guests": 0
+            },
+            {
+                "seats": 4,
+                "guests": 0
+            },
+            {
+                "seats": 4,
+                "guests": 0
+            },
+            {
+                "seats": 4,
+                "guests": 0
+            },
+            {
+                "seats": 4,
+                "guests": 0
+            },
+            {
+                "seats": 0,
+                "guests": 0
+            },
+            {
+                "seats": 0,
+                "guests": 0
+            }
+        ]
+    }`
+}
--- go/mazai.go
+++ go/mazai.go
@@ -1,41 +1,139 @@
 package main
 
-import "fmt"
-import "net/http"
-import "strings"
-import "log"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
 
-func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm() //анализ аргументов,
-    fmt.Println("form:", r.Form)  // ввод информации о форме на стороне сервера
-    fmt.Println("path:", r.URL.Path)
-    fmt.Println("scheme:", r.URL.Scheme)
-    fmt.Println("url:", r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
-    }
-    fmt.Fprintf(w, "Hello World!") // отправляем данные на клиентскую сторону
+	"github.com/gorilla/mux"
+)
+
+const (
+	STATIC_DIR = "/static/"
+	PORT       = "8080"
+)
+
+var (
+	tripTemplate = template.Must(template.ParseFiles("templates/trip.html"))
+)
+
+type TripPageData struct {
+	ID     string
+	METHOD string
+}
+
+func GetHomePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/index.html")
+}
+
+func GetTripPage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	data := TripPageData{ID: vars["id"], METHOD: "POST"}
+	tripTemplate.Execute(w, data)
+}
+
+func GetAllTrips(w http.ResponseWriter, r *http.Request) {
+	data, err := get_all_trips()
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+	} else {
+		fmt.Fprintf(w, "%s", data)
+	}
+}
+
+func EditTrip(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Printf("id: %s\n", vars["id"])
+	//GetHomePage(w, r)
+	var trip = make(map[string]interface{})
+
+	trip["time"] = r.FormValue("time")
+	trip["delay"] = r.FormValue("delay")
+	trip["driver"] = r.FormValue("driver")
+	trip["desc"] = r.FormValue("desc")
+	trip["to"] = r.FormValue("to")
+	trip["from"] = r.FormValue("from")
+
+	var days = make([]map[string]interface{}, 7)
+	for i := 0; i < 7; i++ {
+		var day = make(map[string]interface{})
+		day["seats"], _ = strconv.Atoi(r.FormValue(fmt.Sprintf("seats%d", i)))
+		day["guests"] = 0
+		days[i] = day
+	}
+	trip["days"] = days
+
+	var id = r.FormValue("id")
+	if id == "-1" {
+		add_trip(trip)
+	} else {
+		var intID, _ = strconv.Atoi(id)
+		edit_trip(trip, intID)
+	}
+}
+
+func GetTrip(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	if id == -1 {
+		fmt.Fprintf(w, get_new_trip())
+	} else {
+		var trip, _ = get_trip(id)
+		fmt.Fprintf(w, trip)
+	}
+}
+
+func DeleteTrip(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	delete_trip(id)
+}
+
+func DeleteGuest(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	day, _ := strconv.Atoi(vars["day"])
+	update_guest(id, day, -1)
+}
+
+func AddGuest(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	day, _ := strconv.Atoi(vars["day"])
+	update_guest(id, day, 1)
+}
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Server CSS, JS & Images Statically.
+	router.
+		PathPrefix(STATIC_DIR).
+		Handler(http.StripPrefix(STATIC_DIR, http.FileServer(http.Dir("."+STATIC_DIR))))
+
+	return router
 }
 
 func handleRequests() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	var r = NewRouter()
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "templates/index.html")
-	}); 
-	http.HandleFunc("/trips", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, get_all_trips()) 
-	});
-	http.HandleFunc("/trip/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "templates/new.html")
-	}); 
-	
-    err := http.ListenAndServe(":9000", nil) // задаем слушать порт
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	r.HandleFunc("/", GetHomePage).Methods("GET")
+	r.HandleFunc("/trip/{id}/edit", GetTripPage).Methods("GET")
+
+	r.HandleFunc("/trips", GetAllTrips).Methods("GET")
+	r.HandleFunc("/trip/{id}", GetTrip).Methods("GET")
+	r.HandleFunc("/trip/{id}", EditTrip).Methods("POST")
+	r.HandleFunc("/trip/{id}", DeleteTrip).Methods("DELETE")
+
+	r.HandleFunc("/trip/{id}/{day}/guest", AddGuest).Methods("POST")
+	r.HandleFunc("/trip/{id}/{day}/guest", DeleteGuest).Methods("DELETE")
+
+	err := http.ListenAndServe(":"+PORT, r)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
 func main() {
